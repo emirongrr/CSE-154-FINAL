2c336f8e5d022d0f84a2910068485156eabf505b
emirongrr <emirongrr@gmail.com>
2022-04-06 13:59:40 +0300

    portfolio-cards

---
+++ js/projectList.js
@@ -0,0 +1,48 @@
+const projects = [
+  {
+    id: 1,
+    name: "my_website",
+    shortDescription: "my website fore homework",
+    year: "2022",
+    tags: ["all","coding","web"],
+    image: "",
+    url: "https://github.com/emirongrr/webProgramming"
+  },
+  {
+    id: 1,
+    name: "pixelART_generator",
+    shortDescription: "NFT generator",
+    year: "2021",
+    tags: ["all","crypto","python","nft",],
+    image: ".",
+    url: "https://github.com/emirongrr/pixelArt_Generator"
+  },
+  {
+    id: 2,
+    name: "Sandwich Bots",
+    shortDescription: "DEXs",
+    year: "2021",
+    tags: ["all","crypto","smart-contract","ethereum"],
+    image: "",
+    url: ""
+  },
+  {
+    id: 2,
+    name: "The Merge",
+    shortDescription: "",
+    year: "2021",
+    tags: ["all","ethereum","crypto"],
+    image: "",
+    url: ""
+  },
+  {
+    id: 2,
+    name: "Oracle",
+    shortDescription: "",
+    year: "2021",
+    tags: ["all","crypto","ethereum"],
+    image: "",
+    url: ""
+  },
+  
+]
